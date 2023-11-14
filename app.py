23b02f7801d8fdc91f2d83a8c91db7352651cfa4
Leafblower <ibnkhaqim@gmail.com>
2023-11-15 03:13:17 +0700

    coba coba

--- app.py
+++ app.py
@@ -104,8 +104,9 @@ def main():
             
             #Menampilkan perbandingan radar chart antara lagu yang dimasukkan dengan 5 lagu teratas
             fig_radar = radar_chart(dat, song_features_normalized)
-            st.markdown("### Gambaran Kemiripan Ciri-Ciri lagumu dengan Rekomendasinya!")            
-            st.plotly_chart(fig_radar)
+            st.markdown("### Gambaran Kemiripan Ciri-Ciri lagumu dengan Rekomendasinya!") 
+            fig_radar           
+            # st.plotly_chart(fig_radar)
 
 if __name__ == "__main__":
     main()
